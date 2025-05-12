3620ee0572f3e0820bb0172cf5fbeb6c15af065a
Mijndert Stuij <mijndert@mijndertstuij.nl>
2025-05-12 13:54:57 +0200

    Rewrites file structure to use tfvars

---
+++ platform/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.50.1"
+    }
+
+    talos = {
+      source  = "siderolabs/talos"
+      version = "~> 0.7"
+    }
+  }
+}
---
+++ platform/state.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    use_lockfile                = true
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    bucket                      = "tc-prod-tfstate"
+    key                         = ""
+    region                      = "eu-central-1"
+
+    endpoints = {
+      "s3" = "https://fsn1.your-objectstorage.com"
+    }
+  }
+}
